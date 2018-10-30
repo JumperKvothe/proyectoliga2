4dd314d461edb216c7ad49316b843c01d4520f44
Enrique <qiqe.rionegro@hotmail.es>
2018-10-30 11:42:30 +0100

    Entrar

--- reqriot.js
+++ reqriot.js
@@ -1,19 +1,10 @@
-var RiotRequest = require('riot-lol-api');
+var RiotRequest = require('riot-api');
 
-var riotRequest = new RiotRequest('RGAPI-7b11a6e1-f401-4611-a45a-b74aac374c63');
+var api = new RiotApi('RGAPI-7b11a6e1-f401-4611-a45a-b74aac374c63');
 
-var cache = {
-    get: function(region, endpoint, cb) {
-      // Try to read from cache,
-      // Return cb(null, data) if data is already available in your cache.
-      // If it's a cache-miss, you still need to call cb(null, null) for the request to proceed.
-      // Do not just call cb(null)!
-      cb(null, null);
-    },
-    set: function(region, endpoint, cacheStrategy, data) {
-      // Use this function to store `data`, which is the result of the API call to `endpoint` on `region`.
-    }
-  };
-
-  
-riotRequest.request('euw1', 'JumperKvothe', '/lol/summoner/v3/summoners/by-name/', function(err, data) {});
+api.getSummoner({
+    'region': 'EUW1',
+    'summonerName': 'JumperKvothe'
+}, function(data) {
+   
+});
