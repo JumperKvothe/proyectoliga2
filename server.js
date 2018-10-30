35ba5c3ba770f462fef46559b947ef2d38396f72
Enrique <qiqe.rionegro@hotmail.es>
2018-10-30 12:54:31 +0100

    Consiguiendo sacar información de la API de Rito

--- server.js
+++ server.js
@@ -1,43 +1,28 @@
-var express = require('express');
-var app = express();
-var exphbs = require('express-handlebars');
-var request = require('async');
-var async = require('async');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// setting default platformId to be used if you don't specify it on the endpoint method
+process.env.LEAGUE_API_PLATFORM_ID = 'euw1'
+process.env.LEAGUE_API_KEY = 'RGAPI-7b11a6e1-f401-4611-a45a-b74aac374c63'
 
-app.set('view engine', 'handlebars');
+const LeagueJs = require('./node_modules/leaguejs/lib/LeagueJS.js');
+const leagueJs = new LeagueJs(process.env.LEAGUE_API_KEY);
 
-app.get('/', function(req, res) {
-    var data = {};
-    var api_key = 'RGAPI-7b11a6e1-f401-4611-a45a-b74aac374c63';
-    var s_toSearch= 'JumperKvothe';
-    var URL = 'https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + s_toSearch + '?api_key=' + api_key;
+leagueJs.Summoner
+	.gettingByName('JumperKvothe')
+	.then(data => {
+		'use strict';
+		console.log(data);
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	});
 
-        async.waterfall([
-            function(callback) {
-            request(URL, function(err, response, body) {
-                if(!err && response.statusCode == 200) {
-                var json = JSON.parse(body);
-                data.id = json[s_toSearch].id;
-                data.name = json[s_toSearch].name;
-                callback(null, data);
-                } else {
-                console.log(err);
-                }
-            });
-            }
-        ],
-        function(err, data) {
-            if(err) {
-            console.log(err);
-            return;
-            }
-        
-            res.render('index', {
-            info: data
-            });
-        });
-    });
-
-var port = Number(process.env.PORT || 3000);
-app.listen(port);
+leagueJs.Summoner
+	.gettingByAccount(22177292, 'euw')
+	.then(data => {
+		'use strict';
+		console.log(data);
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	});
