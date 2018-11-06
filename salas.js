f56d04b47b543888cf8a027f06f43d8921056ec0
Enrique <qiqe.rionegro@hotmail.es>
2018-11-06 13:05:09 +0100

    entrar

--- salas.js
+++ salas.js
@@ -1,4 +1,6 @@
-function crearsala()
-{
-    
+function crearSala(){
+
+    /* $("#right").append('<li class="hover" id="' + i + '" onclick="abrirchat(' + i + ')">' + obj[i] + '</li>'); */
+    $("#right").append('<div style="border: 1px solid white;"><p>EEE</p></div>')
+    $("#left").append('<div style="border: 1px solid white;"><p>EEE</p></div><br>')
 }
