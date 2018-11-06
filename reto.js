430274268f3c0dbda905b6bce432130b7f1f95c0
John Doe <johndoe@example.com>
2018-11-06 12:19:26 +0100

    entrar

---
+++ reto.js
@@ -0,0 +1,6 @@
+function crearSala(){
+
+    /* $("#right").append('<li class="hover" id="' + i + '" onclick="abrirchat(' + i + ')">' + obj[i] + '</li>'); */
+    $("#right").append('<div style="border: 1px solid black;"><p>EEE</p></div>')
+    $("#left").append('<div style="border: 1px solid black;"><p>EEE</p></div><br>')
+}
