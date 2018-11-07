c72516817d1778624113a6c303069cc5320cb7d7
John Doe <johndoe@example.com>
2018-11-07 10:47:22 +0100

    entrar

--- funciones.js
+++ funciones.js
@@ -1,3 +1,7 @@
+//Variables Globales
+nombre = "paco"
+let puntos = 500
+
 //Funciones generales
 
 function gotoindex(){
@@ -6,10 +10,20 @@ function gotoindex(){
 
 function gotomodojuego(){
     window.location.href = "modojuego.html"
+    guardarDatos()
 }
 
 function goto1vs1 (){
     window.location.href = "1vs1.html"
+}
+
+//Funciones index
+
+function guardarDatos(){
+    nombre = ("#name").html
+    //puntos = ("#points").html
+    puntos = 700
+    console.log(puntos)
 }
 
 //Funciones goto1vs1
@@ -19,4 +33,5 @@ function crearSala(){
     $("#right").append('<div style="border: 1px solid white;"><p>EEE</p></div>')
     $("#left").append('<div style="border: 1px solid white;"><p>EEE</p></div><br>')
     $("#crear").hide()
+    console.log(nombre, puntos)
 }
