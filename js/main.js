98a82bb0900c9faa32f011195ce02434842c3973
Enrique <qiqe.rionegro@hotmail.es>
2018-11-16 14:54:39 +0100

    Popper fallo con el builder

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow()
     {
-        win = new BrowserWindow ({width: 800, height: 600})
+        win = new BrowserWindow ({width: 800, height: 600, webPreferences: {devTools: false}})
         win.loadURL(url.format({
             pathname: path.join(__dirname,'../html/index.html'),
             protocol: 'file',
