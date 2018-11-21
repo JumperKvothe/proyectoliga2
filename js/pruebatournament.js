974384ff69865815bf126ea4856c2606acaade5f
John Doe <johndoe@example.com>
2018-11-21 14:52:23 +0100

    Entrar

--- js/pruebatournament.js
+++ js/pruebatournament.js
@@ -2,6 +2,38 @@ process.env.LEAGUE_API_PLATFORM_ID = 'euw1'
 process.env.LEAGUE_API_KEY = 'RGAPI-bf61a41d-fbf1-411f-aaf2-3fd56e9ee12e'
 
 const Kayn = require('../node_modules/kayn/lib/Kayn.js');
-const kayn = Kayn(process.env.LEAGUE_API_KEY);
+const kayn = new Kayn(process.env.LEAGUE_API_KEY);
 
-kayn.summonerName(JumperKvothe);
+kayn.Summoner
+    .summonerName('JumperKvothe')
+	.then(data => {
+        'use strict';
+        
+        console.log(data);
+    })
+    .catch(err => {
+		'use strict';
+		console.log(err);
+	});
+
+
+/* leagueJs.Summoner
+	.gettingByName('JumperKvothe')
+	.then(data => {
+        'use strict';
+        
+        console.log(data);
+        //Prueba de coger un dato (Funciona). Para probar todo usar ' node server.js '
+        const ids = data.id;
+        console.log(ids);
+        
+        $( document ).ready(function() {
+			$(".nombre").html(data.name);
+			loluser = data.name;
+        });
+        
+	})
+	.catch(err => {
+		'use strict';
+		console.log(err);
+	}); */
