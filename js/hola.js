fe8ae1a675cce619a5ef9e4c12e91a4f2975e067
John Doe <danireysanchez@gmail.com>
2019-01-22 00:36:04 +0100

    ipc

--- js/hola.js
+++ js/hola.js
@@ -23,3 +23,34 @@ module.exports = {
 }
 
 //https://github.com/hakynseyer
+
+/* //Mensaje Síncrono - Renderer.js
+const syncMsgBtn = document.getElementById('sendSyncMsgBtn')
+syncMsgBtn.addEventListener('click', function () {
+    const reply = ipc.sendSync('synchronous-message', 'Mr. Watson, come here.')
+    const message = `Synchronous message reply: ${reply}`
+    document.getElementById('syncReply').innerHTML = message
+})
+
+//Mensaje Asíncrono - Renderer.js
+const asyncMsgBtn = document.getElementById('sendAsyncMsgBtn')
+asyncMsgBtn.addEventListener('click', function () {
+    ipc.send('asynchronous-message', "That's one small step for man")
+})
+
+ipc.on('asynchronous-reply', function (event, arg) {
+    const message = `Asynchronous message reply: ${arg}`
+    document.getElementById('asyncReply').innerHTML = message
+}) */
+
+/* //Mensaje Síncrono - Main.js
+ipc.on('synchronous-message', function (event, arg) {
+    event.returnValue = 'I heard you!'
+})
+  
+//Mensaje Asíncrono - Main.js
+ipc.on('asynchronous-message', function (event, arg) {
+    if (arg === "That's one small step for man") {
+    event.sender.send('asynchronous-reply', ', one giant leap for mankind.')
+    }
+}) */
